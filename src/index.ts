77fd60a703e95cfc011c41b91c35fdefdb190ffb
nick thomas <nichoth@gmail.com>
2023-06-09 11:51:01 -0700

    tests

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,25 @@
-export function example ():void {
-    console.log('hello')
+import type { Crypto } from '@oddjs/odd'
+import { create as createMsg, SignedRequest } from '@ssc-hermes/message'
+import { v4 as uuid } from 'uuid'
+
+export interface Invitation {
+    from:string  // username
+    code:string
+}
+
+export type SignedInvitation = SignedRequest<Invitation>
+
+/**
+ * Create an invitation. This will create and sign a message with the given crypto
+ * keys and username passed in.
+ * @param {Crypto.Implementation} crypto Fission crypto object
+ * @param {string} username The username that created the invitation
+ * @returns {SignedInvitation}
+ */
+export async function create (
+    crypto:Crypto.Implementation,
+    username:string
+):Promise<SignedInvitation> {
+    const code = uuid()
+    return createMsg(crypto, { code, from: username })
 }
--- test/index.ts
+++ test/index.ts
@@ -1,5 +1,23 @@
 import { test } from 'tapzero'
+import * as odd from '@oddjs/odd'
+import { Program } from '@oddjs/odd'
+import { create } from '../dist/index.js'
+import { components } from '@ssc-hermes/node-components'
 
-test('example', async t => {
-    t.ok('ok', 'should be an example')
+let program:Program
+test('setup', async t => {
+    program = await odd.assemble({
+        namespace: { creator: 'test', name: 'testing' },
+        debug: false
+    }, components)
+
+    t.ok(program, 'create a program')
+})
+
+test('create an invitation', async t => {
+    const { crypto } = program.components
+    const inv = await create(crypto, 'alice')
+    t.ok(inv.signature, 'should sign the new invitation')
+    t.ok(inv.author.includes('did:key'), 'should include the signing DID')
+    t.equal(inv.from, 'alice', 'should include the username')
 })
